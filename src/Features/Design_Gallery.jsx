4050ec2a3a5db7d7768e6d148d17da6aa82b968c
Ongeri123 <ongerinewton10@gmail.com>
2025-10-15 16:18:48 +0300

    added design gallery section

---
+++ src/Features/Design_Gallery.jsx
@@ -0,0 +1,18 @@
+function Design_Gallery() {
+    return (
+        <div style={{
+                backgroundImage: "url('/bg.png')",
+                backgroundSize: 'cover',
+                height: '100vh',
+                padding: '80px 20px 20px',
+            }}>
+            <h1 style={{textAlign: 'center', marginBottom: '50px'}}>Design_Gallery</h1>
+            <p>Bold Graphics. Clear Impact.</p>
+            <p>A showcase of visual work that blends cultural flair, sleek branding, and user-focused design.</p>
+        </div>
+    )
+}
+
+
+
+export default Design_Gallery;
